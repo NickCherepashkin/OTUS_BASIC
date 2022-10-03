3da9ef6449c576d0d4631a9343b918e735789790
Nika Drozdova <cherepawka13@mail.ru>
2022-10-03 14:43:29 +0300

    add empty FilmsProject

---
+++ FilmsProject/app/src/main/java/com/drozdova/otus_basic/filmsproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.drozdova.otus_basic.filmsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
