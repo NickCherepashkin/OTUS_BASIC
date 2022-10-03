37244a3b731b6e296037fe50468675abd660c5bf
Nika Drozdova <cherepawka13@mail.ru>
2022-10-03 14:22:10 +0300

    add code from class_work less_5

--- BaseWidgets/app/src/main/java/com/drozdova/otus_basic/basewidgets/MainActivity.java
+++ BaseWidgets/app/src/main/java/com/drozdova/otus_basic/basewidgets/MainActivity.java
@@ -3,12 +3,23 @@ package com.drozdova.otus_basic.basewidgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @BindView(R.id.test)
+//    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+//        text.setText("fffffff");
     }
 }
